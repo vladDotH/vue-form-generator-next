34e41ef0392ad707dd5e12cd86d95d1c704e7690
Vlad <vladbolkunovv@gmail.com>
2023-03-22 01:00:03 +0300

    feat: dev app

---
+++ dev/src/main.ts
@@ -0,0 +1,14 @@
+import Vue, {configureCompat} from "vue";
+import VueFormGenerator from "../../src";
+
+Vue.use(VueFormGenerator);
+
+// import { configureCompat } from "@vue/compat";
+
+configureCompat({MODE:2});
+
+import App from "./app.vue";
+
+new Vue({
+	...App
+}).$mount("#app");
