9bf4a32a38467df4cfac3f44684fca06d6659a9d
Vlad <vladbolkunovv@gmail.com>
2023-03-22 01:00:03 +0300

    feat: dev app

---
+++ dev/src/vue-compat.d.ts
@@ -0,0 +1,11 @@
+declare module 'vue' {
+  import { CompatVue } from 'vue'
+  const Vue: CompatVue
+  export default Vue
+  // eslint-disable-next-line vue/prefer-import-from-vue
+  export * from '@vue/runtime-dom'
+  const { configureCompat } = Vue
+  export { configureCompat }
+}
+
+declare module '../../src'
