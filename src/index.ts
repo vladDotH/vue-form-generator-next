4d580f821b64fb939776e60c2152e9a378d16463
Vlad <vladbolkunovv@gmail.com>
2023-03-26 21:14:33 +0300

    feat: index ts

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,28 @@ import * as schema from "./utils/schema";
 import validators from "./utils/validators";
 import fieldComponents from "./utils/fieldsLoader";
 import abstractField from "./fields/abstractField";
+import { FieldSchema } from "./fields/fields";
 
-const install = (Vue, options) => {
+export type ValidatorFunction<M = any, V = any> = (
+  value: V,
+  field: FieldSchema<M, V>,
+  model: M,
+  messages?: Record<string, string>
+) => any[];
+
+export interface VFGPluginOptions {
+  validators: {
+    [key: string]: ValidatorFunction;
+  };
+}
+
+const install = (Vue: any, options: VFGPluginOptions) => {
   Vue.component("VueFormGenerator", component);
   if (options && options.validators) {
     for (const key in options.validators) {
-      if ({}.hasOwnProperty.call(options.validators, key)) {
-        validators[key] = options.validators[key];
-      }
+      // if ({}.hasOwnProperty.call(options.validators, key)) {
+      validators[key] = options.validators[key];
+      // }
     }
   }
 };
