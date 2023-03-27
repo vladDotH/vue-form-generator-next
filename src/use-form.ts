e29134b83054b5a29784c4a19587fa0949e10ac8
Vlad <vladbolkunovv@gmail.com>
2023-03-28 00:40:11 +0300

    fix(use-form): typing

--- src/use-form.ts
+++ src/use-form.ts
@@ -7,7 +7,7 @@ import {
   isString
 } from "lodash";
 import { getCurrentInstance, Ref } from "vue";
-import { FormOptions } from "./fields/fields";
+import { FieldSchema, FormOptions } from "./fields/fields";
 
 // TODO: drop context binding to disabled, readonly, featured, required
 export const useForm = (
@@ -17,7 +17,7 @@ export const useForm = (
 ) => {
   const instance = getCurrentInstance();
 
-  const getFieldRowClasses = (field) => {
+  const getFieldRowClasses = (field: FieldSchema) => {
     const hasErrors = fieldErrors(field).length > 0;
     const baseClasses = {
       [objGet(options.value, "validationErrorClass", "error")]: hasErrors,
@@ -41,14 +41,14 @@ export const useForm = (
     return baseClasses;
   };
 
-  const fieldErrors = (field) => {
+  const fieldErrors = (field: FieldSchema) => {
     return errors.value
       .filter((e) => e.field === field)
       .map((item) => item.error);
   };
 
   // Get disabled attr of field
-  const fieldDisabled = (field) => {
+  const fieldDisabled = (field: FieldSchema) => {
     if (isFunction(field.disabled))
       return field.disabled.call(instance, model.value, field, instance);
 
@@ -58,7 +58,7 @@ export const useForm = (
   };
 
   // Get readonly prop of field
-  const fieldReadonly = (field) => {
+  const fieldReadonly = (field: FieldSchema) => {
     if (isFunction(field.readonly))
       return field.readonly.call(instance, model.value, field, instance);
 
@@ -68,7 +68,7 @@ export const useForm = (
   };
 
   // Get featured prop of field
-  const fieldFeatured = (field) => {
+  const fieldFeatured = (field: FieldSchema) => {
     if (isFunction(field.featured))
       return field.featured.call(instance, model.value, field, instance);
 
@@ -78,7 +78,7 @@ export const useForm = (
   };
 
   // Get required prop of field
-  const fieldRequired = (field) => {
+  const fieldRequired = (field: FieldSchema) => {
     if (isFunction(field.required))
       return field.required.call(instance, model.value, field, instance);
 
