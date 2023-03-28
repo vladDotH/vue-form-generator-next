828f3d5b8ac8be775cf5744d928163c4fce8ec3e
Vlad <vladbolkunovv@gmail.com>
2023-03-28 12:25:46 +0300

    feat: form props interface & object

--- src/form.ts
+++ src/form.ts
@@ -1,4 +1,46 @@
-import { FieldSchema, FormOptions } from "./fields/fields";
+import { FieldSchema, FormOptions, FormSchema } from "./fields/fields";
+
+export interface FormProps {
+  schema: FormSchema;
+  model: any;
+  option: FormOptions;
+  multiple: boolean;
+  isNewModel: boolean;
+  tag: string;
+}
+
+export const FormPropsObject = {
+  schema: Object,
+  model: Object,
+  options: {
+    type: Object,
+    default() {
+      return {
+        validateAfterLoad: false,
+        validateAfterChanged: false,
+        validateDisabled: false,
+        validateReadonly: false,
+        fieldIdPrefix: "",
+        validateAsync: false,
+        validationErrorClass: "error",
+        validationSuccessClass: ""
+      };
+    }
+  },
+  multiple: {
+    type: Boolean,
+    default: false
+  },
+  isNewModel: {
+    type: Boolean,
+    default: false
+  },
+  tag: {
+    type: String,
+    default: "fieldset",
+    validator: (value: string) => value.length > 0
+  }
+};
 
 export interface WrapperProps<M = any> {
   vfg: any;
