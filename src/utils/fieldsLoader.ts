890d05f3751601c5b3c199f37c31187ca7dbc4db
Vlad <vladbolkunovv@gmail.com>
2023-04-01 23:44:28 +0300

    feat: checkbox & submit load

--- src/utils/fieldsLoader.ts
+++ src/utils/fieldsLoader.ts
@@ -1,9 +1,13 @@
 import FieldTextArea from "../fields/core/FieldTextArea.vue";
 import FieldInput from "../fields/core/FieldInput.vue";
+import FieldCheckbox from "../fields/core/FieldCheckbox.vue";
+import FieldSubmit from "../fields/core/FieldSubmit.vue";
 
 const fieldComponents = {
   FieldTextArea,
-  FieldInput
+  FieldInput,
+  FieldCheckbox,
+  FieldSubmit
 };
 
 export default fieldComponents;
