6a469e1d7bc3798b51f02a2de734e215c36dfda4
vlad <vladbolkunovv@gmail.com>
2023-05-11 01:43:19 +0300

    feat: lib build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,45 @@
 import { fileURLToPath, URL } from "node:url";
-
+import dts from "vite-plugin-dts";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  build: {
+    sourcemap: true,
+    minify: false,
+    commonjsOptions: {
+      esmExternals: true
+    },
+    lib: {
+      entry: "./src/index.ts",
+      name: "vfg-next",
+      formats: ["es", "cjs", "umd"],
+      fileName: (format) => `vfg-next.${format}.js`
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        exports: "named",
+        assetFileNames: `vfg-next.[ext]`,
+        globals: {
+          vue: "Vue"
+        }
+      }
+    }
+  },
+
+  css: { preprocessorOptions: { scss: { charset: false } } },
+
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/]
+    }),
+    dts({
+      tsConfigFilePath: "./tsconfig.json",
+      insertTypesEntry: true
+    })
+  ],
 
   resolve: {
     alias: {
